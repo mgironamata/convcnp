f26702e3f1db6606528edbfc70f0c06edbd3502e
Marc Girona-Mata <mgirona29@gmail.com>
2020-07-16 15:51:04 +0100

    changes to data loader

--- convcnp/data_loader_pytorch.py
+++ convcnp/data_loader_pytorch.py
@@ -2,31 +2,22 @@ from torch.utils.data import DataLoader, Dataset
 
 class HydroDataset(Dataset):
     
-    def __init__(self):
+    def __init__(self,gen,num_tasks_epoch):
         pass
         #self.task = gen.generate_task()
         #self.n_samples = self.task['x'].shape[0]
         #self.task['x'].shape[0]
+        self.gen = gen
+        self.num_tasks_epoch = num_tasks_epoch
         
     def __getitem__(self,index):
         """task_dict = {}
+        
         for k in self.task.keys():
             #exec("%s = task[k]" % k)
             task_dict.update( {str(k) : self.task[k][index]} )
         return gen.generate_task"""
-        return gen.generate_task()
+        return self.gen.generate_task()
         
     def __len__(self):
-        return 1 #self.n_samples
-
-start = time.time()
-dataset = HydroDataset()
-elapsed = time.time() - start
-print(elapsed)
-len(dataset)
-#print(dataset)
-    
-dataloader = DataLoader(dataset=HydroDataset(), batch_size=4, shuffle=False, num_workers=2)
-#dataiter = iter(dataloader)
-#data = dataiter.next()
-#print(len(dataset))
+        return self.num_tasks_epoch
